a5036863267ed697b731a4c40685451b10f8a669
Ahmedsaad121 <ahmedpipo15@gmail.com>
2021-04-29 00:32:13 +0200

    project

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(".carousel").owlCarousel({
+	margin:20,
+	items: 1,
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 5000,
+	autoplayHoverpause: true,
+	responsive: {},
+	dots: !2,
+	nav:true,
+	
+});
